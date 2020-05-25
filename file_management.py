fcf39196865f652f203373aaa622eb67d81c37b3
mdsatria <mdsatria@gmail.com>
2020-05-25 20:27:04 +0800

    commit

---
+++ file_management.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+import re
+import shutil
+
+def convert_bytes(num):
+    """
+    this function will convert bytes to MB.... GB... etc
+    """
+    for x in ['bytes', 'KB', 'MB', 'GB', 'TB']:
+        if num < 1024.0:
+            return "%3.1f %s" % (num, x)
+        num /= 1024.0
+
+def manage_file(directory, keyword, new_directory):
+    """
+    Organize files by the filename pattern
+    ---------
+    Parameters :
+    directory = directory with file need to be organised. example D:/folder
+    keyword = any char or combination of char that filename might contain
+    new_directory = any file that meet condition from keyword will be moved in new_directory
+    """
+    p = Path(directory)
+    files = []
+    for i in p.glob('**/*'):
+        files.append(i)    
+    
+    regexp = re.compile(r'.*({}).*'.format(keyword), re.IGNORECASE)
+    count = 0
+    for i in files:
+        if (regexp.search(str(i))):
+            count += 1
+            shutil.move(str(i), new_directory)
+
+    return '{} files that contain keyword \'{}\' have been moved to {}'.format(count, keyword, new_directory)
+
+directory = ''
+keyword = ''
+new_directory = ''
+manage_file(directory, keyword, new_directory)
---
+++ generate_img_data.py
@@ -0,0 +1,55 @@
+from pathlib import Path
+from PIL import Image
+import numpy as np
+
+def get_rgbDset(directory, nrow, ncol, imgtype='jpg', save='no', r_name='x_r.csv', g_name='x_g.csv', b_name='x_b.csv', label_name='y.csv'):
+    """
+    Read multiple RGB images and return 4D numpy array. 
+    Image must JPG file.
+    General assumption each class separated in different folder
+    -------
+    Parameters
+    directory : directory containing images. image with different label should be located in different folder
+    nrow : width of desired image ouput
+    ncol : height of desired image ouput
+    imgtype : file extension of image. default is jpg
+    """
+    # define image file extension
+    imgtype = '*.' + imgtype
+    # get all folder/directory in path
+    dirname = Path(directory)
+    folder = [f.name for f in dirname.iterdir() if f.is_dir()]
+    # create variable to save label and image
+    label = []    
+    images = np.zeros((1,nrow,ncol,3), 'uint8')
+
+    for i in range (len(folder)):
+        for filename in Path(directory+folder[i]).rglob(imgtype):
+            image = np.array(Image.open(filename).resize((nrow,ncol)))
+            image = np.expand_dims(image, axis=0)
+            images = np.concatenate((images,image), axis=0)
+            label.append(folder[i])    
+    images = np.delete(images, (0), axis=0)
+    #label = np.array(label)
+    
+    if (save=='no'):
+        return images, label
+    elif (save=='yes'):
+        r = images[:,:,:,0].reshape(-1, nrow*ncol)
+        g = images[:,:,:,1].reshape(-1, nrow*ncol)
+        b = images[:,:,:,2].reshape(-1, nrow*ncol)
+        np.savetxt(r_name, r.astype(int), delimiter=',', fmt='%d')
+        np.savetxt(g_name, g.astype(int), delimiter=',', fmt='%d')
+        np.savetxt(b_name, b.astype(int), delimiter=',', fmt='%d')
+        np.savetxt(label_name, label, delimiter=',')
+
+        return 'images data and its label are saved in csv files'
+
+def construct_image(r, g, b, ncol, nrow):
+    r = r.reshape(ncol, nrow).astype('uint8')
+    g = g.reshape(ncol, nrow).astype('uint8')
+    b = b.reshape(ncol, nrow).astype('uint8')
+
+    img = np.dstack((r, g, b))
+
+    return img 
