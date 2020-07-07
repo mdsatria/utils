a2e0c64619743ecf2044e551c4f453ef25a1f65d
Made Satria Wibawa <mdsatria@gmail.com>
2020-07-07 17:51:49 +0800

    fix

--- file_management.py
+++ file_management.py
@@ -10,6 +10,7 @@ def convert_bytes(num):
         if num < 1024.0:
             return "%3.1f %s" % (num, x)
         num /= 1024.0
+    return num
 
 def manage_file(directory, keyword, new_directory):
     """
@@ -33,16 +34,40 @@ def manage_file(directory, keyword, new_directory):
     
     regexp = re.compile(r'.*({}).*'.format(keyword), re.IGNORECASE)
     count = 0
-    for i in files:
+    
+    for i in files:        
         if (regexp.search(str(i))):
-            count += 1
-            shutil.move(str(i), new_directory)
-
+            filename = Path(i).name
+            try:
+                shutil.move(str(i), new_directory)
+                count += 1
+            except OSError:
+                print("File {} already exist in new directory. ".format(filename))
+                
+                old_size = convert_bytes(Path(i).stat().st_size)
+                path_new = '{}{}'.format(new_directory, filename)
+                new_size = convert_bytes(Path(path_new).stat().st_size)
+                
+                print("File size in old directory: {}. File size in new directory: {}".format(old_size, new_size))
+                print("Replace it ? y/n")
+                
+                inp = str(input()).lower()
+                if(inp == 'y'):
+                    shutil.move(str(i), '{}{}.'.format(new_directory,filename))
+                    count += 1
+                elif(inp == 'n'):
+                    print("File not moved")
+                else:
+                    print("Wrong input !")
+                    
+                    
     return print('{} files that contain keyword \'{}\' have been moved to {}'.format(count, keyword, new_directory))
 
-"""Usage
-directory = 'some path'
-keyword = 'some path'
-new_directory = ''
-manage_file(directory, keyword, new_directory)
-"""
+if __name__ == '__main__':
+    print("Enter search directory ")
+    directory = str(input())
+    print("Enter new directory")
+    new_directory = str(input())
+    print("Enter keyword")
+    keyword = str(input())
+    manage_file(directory, keyword, new_directory)
